27d0ef2ec46b5f37fce4bcea922cebcbb97f8546
Christine Augustine and Rachel Schieferstein <christine+rachel@>
2020-02-03 11:49:28 -0800

    lint working successfully

---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin'); //new line
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');  // new line
+
+
+module.exports = {
+    entry: './src/main.js',
+    output: {
+      filename: 'bundle.js',
+      path: path.resolve(__dirname, 'dist')
+    },
+    devtool: 'eval-source-map',  // new line
+    devServer: {                 // new line
+      contentBase: './dist'      // new line
+    },
+  plugins: [
+    new UglifyJsPlugin({sourceMap: true}),    // new line
+    new CleanWebpackPlugin(),   // new line 
+    new HtmlWebpackPlugin({
+      title: 'Ping Pong',
+      template: './src/index.html',
+      inject: 'body'
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "eslint-loader"
+      }
+    ]
+  }
+};
